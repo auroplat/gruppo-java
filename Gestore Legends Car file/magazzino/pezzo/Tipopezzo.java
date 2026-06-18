84cbf0990d15d1a561b9e3efa283a9052a52a0dc
Raffaeletur <raffaele.turone01@universitadipavia.it>
2026-06-18 22:39:41 +0200

    Add TipoPezzo enum for car parts

---
+++ Gestore Legends Car file/magazzino/pezzo/Tipopezzo.java
@@ -0,0 +1,14 @@
+package magazzino.pezzo;
+
+public enum TipoPezzo {
+    MOTORE,
+    FRENI,
+    GOMME,
+    SOSPENSIONI,
+    CARROZZERIA,
+    ELETTRONICA,
+    OLIO,
+    FILTRO,
+    BATTERIA,
+    ALTRO
+}

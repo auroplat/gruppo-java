19dfe7dd234f21681955b643be0c3d6178a55392
ledion474 <ledion.caka01@univeristadipavia.it>
2026-07-10 18:36:08 +0200

    .

--- Gestore Legends Car/src/it/unipv/bitFactory/controller/GestionePrenotazioniController.java
+++ Gestore Legends Car/src/it/unipv/bitFactory/controller/GestionePrenotazioniController.java
@@ -1,5 +1,36 @@
 package it.unipv.bitFactory.controller;
 
-public class GestionePrenotazioniController {
+import it.unipv.bitFactory.prenotazioni.Cliente;
+import it.unipv.bitFactory.prenotazioni.Evento;
+import it.unipv.bitFactory.prenotazioni.SistemaPrenotazioni;
+import it.unipv.bitFactory.veicoli.Legends;
 
+public class PrenotazioneController {
+
+    private SistemaPrenotazioni sistema;
+
+    public PrenotazioneController() {
+
+        sistema = new SistemaPrenotazioni();
+    }
+
+    public String prenota(Cliente cliente,
+                          Evento evento,
+                          Legends auto) {
+
+        return sistema.effettuaPrenotazione(
+                cliente,
+                evento,
+                auto
+        );
+    }
+
+    public String annullaPrenotazione(Cliente cliente,
+                                      Evento evento) {
+
+        return sistema.annullaPrenotazione(
+                cliente,
+                evento
+        );
+    }
 }

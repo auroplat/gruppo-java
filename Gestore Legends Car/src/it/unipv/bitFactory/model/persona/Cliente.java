4a416d8083e7ecc4f49b3accae8b627889c3a597
ledion474 <ledion.caka01@univeristadipavia.it>
2026-07-10 18:39:44 +0200

    .

---
+++ Gestore Legends Car/src/it/unipv/bitFactory/model/persona/Cliente.java
@@ -0,0 +1,38 @@
+package it.unipv.bitFactory.prenotazioni;
+
+import it.unipv.bitFactory.utenti.Persona;
+
+public class Cliente extends Persona {
+
+    private boolean patenteValida;
+    private int eta;
+
+    public Cliente(String nome,
+                   String cognome,
+                   boolean patenteValida,
+                   int eta,
+                   String telefono,
+                   String email) {
+
+        super(nome, cognome, telefono, email);
+
+        this.patenteValida = patenteValida;
+        this.eta = eta;
+    }
+
+    public boolean isPatenteValida() {
+        return patenteValida;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    @Override
+    public String toString() {
+
+        return super.toString() +
+                " | Patente: " + patenteValida +
+                " | Età: " + eta;
+    }
+}

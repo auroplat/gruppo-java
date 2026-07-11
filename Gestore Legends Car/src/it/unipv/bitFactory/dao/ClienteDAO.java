8ed52b7fa02b7278b35122f38eae3102c8a90d75
auroplat <aurora.platino01@universitadipavia.it>
2026-07-11 17:19:05 +0200

    modifiche interfacce dao

---
+++ Gestore Legends Car/src/it/unipv/bitFactory/dao/ClienteDAO.java
@@ -0,0 +1,17 @@
+package it.unipv.bitFactory.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import it.unipv.bitFactory.model.persona.Cliente;
+
+public interface ClienteDAO {
+
+    List<Cliente> caricaClienti();
+
+    Optional<Cliente> cercaPerEmail(String email);
+
+    boolean salva(Cliente cliente);
+
+    boolean elimina(String email);
+}
---
+++ Gestore Legends Car/src/it/unipv/bitFactory/dao/PrenotazioneDAO.java
@@ -0,0 +1,27 @@
+package it.unipv.bitFactory.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import it.unipv.bitFactory.model.prenotazioni.Prenotazione;
+
+public interface PrenotazioneDAO {
+
+    List<Prenotazione> caricaPrenotazioni();
+
+    List<Prenotazione> cercaPerEvento(String nomeEvento);
+
+    List<Prenotazione> cercaPerCliente(String emailCliente);
+
+    Optional<Prenotazione> cerca(
+            String nomeEvento,
+            String emailCliente
+    );
+
+    boolean aggiungi(Prenotazione prenotazione);
+
+    boolean elimina(
+            String nomeEvento,
+            String emailCliente
+    );
+}

1ae8b77c2a68430432276eb580f17409915e0306
Raffaeletur <raffaele.turone01@universitadipavia.it>
2026-07-13 15:29:19 +0200

    Modifica classi

--- Gestore Legends Car/src/it/unipv/bitFactory/dao/MagazzinoDAO.java
+++ Gestore Legends Car/src/it/unipv/bitFactory/dao/MagazzinoDAO.java
@@ -1,5 +1,20 @@
-package it.unipv.bitFactory.dao;
+package it.unipv.bitFactory.dao.magazzino;
 
-public class MagazzinoDAO {
+import java.util.List;
+import java.util.Optional;
 
+import it.unipv.bitFactory.model.magazzino.VoceMagazzino;
+import it.unipv.bitFactory.model.pezzi.Pezzo;
+
+public interface MagazzinoDAO {
+
+    Optional<VoceMagazzino> trovaPerIdPezzo(String idPezzo);
+
+    List<VoceMagazzino> trovaTutti();
+
+    List<VoceMagazzino> trovaDisponibili();
+
+    void inserisciPezzo(Pezzo pezzo);
+
+    void eliminaPezzo(String idPezzo);
 }
--- Gestore Legends Car/src/it/unipv/bitFactory/dao/VeicoloDAO.java
+++ Gestore Legends Car/src/it/unipv/bitFactory/dao/VeicoloDAO.java
@@ -1,5 +1,31 @@
-package it.unipv.bitFactory.dao;
+package it.unipv.bitFactory.dao.veicoli;
 
-public class VeicoloDAO {
+import java.util.List;
+import java.util.Optional;
 
+import it.unipv.bitFactory.model.pezzi.Pezzo;
+import it.unipv.bitFactory.model.pezzi.TipoPezzo;
+import it.unipv.bitFactory.model.veicoli.Legends;
+
+public interface VeicoloDAO {
+
+    void inizializzaDatabase();
+
+    boolean esisteVeicolo(String idVeicolo);
+
+    int contaPezziLiberi(TipoPezzo tipoPezzo);
+
+    List<Pezzo> trovaPezziLiberi(TipoPezzo tipoPezzo, int quantitaRichiesta);
+
+    Optional<Pezzo> trovaPezzoLiberoPerId(String idPezzo);
+
+    void salvaLegends(Legends legends);
+
+    void sostituisciPezzo(String idVeicolo, String idPezzoVecchio, String idPezzoNuovo);
+
+    void eliminaVeicolo(String idVeicolo);
+
+    Optional<Legends> trovaLegendsPerId(String idVeicolo);
+
+    List<Legends> trovaTutteLegends();
 }

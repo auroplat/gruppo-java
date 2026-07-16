20652dba236fc88db511699f76b9ed949dc2d6d8
auroplat <aurora.platino01@universitadipavia.it>
2026-07-17 01:04:08 +0200

    pulizia

--- Gestore Legends Car/src/it/unipv/bitFactory/controller/GestioneSessioniController.java
+++ Gestore Legends Car/src/it/unipv/bitFactory/controller/GestioneSessioniController.java
@@ -11,36 +11,21 @@ public final class GestioneSessioniController {
 
     private final ServizioSessioni sessioniService;
 
-    public GestioneSessioniController(
-            ServizioSessioni sessioniService
-    ) {
-        this.sessioniService = Objects.requireNonNull(
-                sessioniService,
-                "Il service delle sessioni non può essere null"
-        );
+    public GestioneSessioniController(ServizioSessioni sessioniService) {
+    	
+        this.sessioniService = Objects.requireNonNull(sessioniService, "Il service delle sessioni non può essere null");
     }
 
-    public void registraSessione(
-            String idMacchina,
-            Sessione sessione
-    ) {
-        sessioniService.registraSessione(
-                idMacchina,
-                sessione
-        );
+    public void registraSessione( String idMacchina, Sessione sessione) {
+        sessioniService.registraSessione(idMacchina, sessione);
     }
 
     public List<String> elencaIdMacchine() {
         return sessioniService.elencaIdMacchine();
     }
 
-    public void registraSessioneEsterna(
-            String idMacchina,
-            SessioneEsterna sessioneEsterna
-    ) {
-        sessioniService.registraSessioneEsterna(
-                idMacchina,
-                sessioneEsterna
-        );
+    public void registraSessioneEsterna(String idMacchina, SessioneEsterna sessioneEsterna) {
+        
+    	sessioniService.registraSessioneEsterna(idMacchina, sessioneEsterna);
     }
 }
